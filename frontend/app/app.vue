df467f61ac75ac634b966708b15e13214310ae9c
aliaksei_yuryeu <aliaksei.yuryeu@outsource.aerones.com>
2025-12-25 13:51:57 +0100

    feat: init tailwind

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex h-screen w-screen justify-center items-center">
+    <h1 class="text-4xl font-bold">Hello World</h1>
   </div>
 </template>
